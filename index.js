b4e4998419ce2835a3137de2eb5562050fea6823
Aex12 <alexcastonmolero@gmail.com>
2017-08-28 19:03:36 +0200

    Updated

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+var request = require('request');
+
 class CoinMarketCap {
-	
+	constructor(options={}){
+		this.API_URL = options.API_URL || "https://api.coinmarketcap.com/v1/ticker";
+		this.convert = options.convert || "USD";
+	}
+
+	_getJSON(url, callback){
+		request(this.API_URL+url, (error, response, body) => {
+			if(error){ 
+				callback(false);
+				return this;
+			}
+			if(response && response.statusCode == 200){
+				callback(JSON.parse(body))
+			} else {
+				callback(false);
+				return this;
+			}
+		});
+	}
+
+	get(coin, callback){
+		if(callback){
+			this._getJSON(`/${coin}/?convert=${this.convert}`, callback);
+			return this;
+		} else {
+			return false;
+		}
+	}
+
+	getAll(callback){
+		if(callback){
+			this._getJSON(`/?convert=${this.convert}`, callback);
+			return this;
+		} else {
+			return false;
+		}
+	}
+
+	getTop(top, callback){
+		if(callback){
+			this._getJSON(`/?convert=${this.convert}&limit=${top}`, callback);
+			return this;
+		} else {
+			return false;
+		}
+	}
+
 }
+
+module.exports = CoinMarketCap;
