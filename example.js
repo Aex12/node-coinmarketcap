baa026b871d9eb5bd340b73ffb1efb8e9022608b
Aex12 <alexcastonmolero@gmail.com>
2017-08-28 22:36:48 +0200

    Fixed missing semicolon

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var CoinMarketCap = require(".")
 
 var options = {
-	refresh: 60 // Refresh time in seconds (Default: 60)
+	refresh: 60, // Refresh time in seconds (Default: 60)
 	convert: "EUR" // Convert price to different currencies. (Default USD)
 }
 var coinmarketcap = new CoinMarketCap.events(options); 
@@ -31,4 +31,4 @@ coinmarketcap.onPercentChange1h("BTC", 10, (coin) => {
 // Put event on BTC with no conditions. It will trigger every 60 seconds with information about that coin (Or the defined time in options)
 coinmarketcap.on("BTC", (coin) => {
 	console.log(coin)
-})
+});
